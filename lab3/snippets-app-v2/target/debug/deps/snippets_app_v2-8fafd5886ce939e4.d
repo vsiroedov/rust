6934738c6ac53ffb07f518ab55524456e149fad5
vsiroedov <145013832+vsiroedov@users.noreply.github.com>
2025-12-20 02:54:03 +0200

    Delete lab3/snippets-app-v2/target directory

--- lab3/snippets-app-v2/target/debug/deps/snippets_app_v2-8fafd5886ce939e4.d
+++
@@ -1,5 +0,0 @@
-D:\4 kurs\Rust\rust\lab3\snippets-app-v2\target\debug\deps\snippets_app_v2-8fafd5886ce939e4.d: src\main.rs
-
-D:\4 kurs\Rust\rust\lab3\snippets-app-v2\target\debug\deps\snippets_app_v2-8fafd5886ce939e4.exe: src\main.rs
-
-src\main.rs:
